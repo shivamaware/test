d477bf73d596b7122fcc7c1eb5f307b23a9a0e84
unknown <shivamaware@gmail.com>
2020-05-26 22:29:50 +0530

    first version of MultiplicationTable code

---
+++ MultiplicationTable.java
@@ -0,0 +1,27 @@
+package com.Shivam.FirstJavaPoject;
+
+public class MultiplicationTable {
+	void print()
+	{
+	//for(int i=1;i<=10;i++)
+	{
+		print(5);
+		//System.out.printf("%d * %d = %d", 5 , i , 5*i).println();
+	}
+	}
+	void print(int table)
+	{
+	//for(int i=1;i<=10;i++)
+	{
+		print(table,1,10);
+		//System.out.printf("%d * %d = %d", table , i , table*i).println();
+	}
+	}
+	void print(int table, int from, int to)
+	{
+	for(int i=from;i<=to;i++)
+	{
+		System.out.printf("%d * %d = %d", table , i , table*i).println();
+	}
+	}
+}
