a62155e4ccf9fde90d5ac1ec1df09740793bb098
unknown <shivamaware@gmail.com>
2020-05-27 10:55:52 +0530

    Some more changes

---
+++ MultiplicaionTableRunner.java
@@ -0,0 +1,13 @@
+package com.Shivam.FirstJavaPoject;
+
+public class MultiplicaionTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();	
+		//table.print(6);
+		//table.print(7,11,20);
+		System.out.println("This print line is for practice of GitHub not related to our MultiplicationTableRunner code");
+	}
+
+}
